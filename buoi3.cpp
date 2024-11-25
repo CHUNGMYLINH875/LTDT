4b64b6e15f8acf26c8ce6cc571286c006b42e496
CHUNGMYLINH875 <147479193+CHUNGMYLINH875@users.noreply.github.com>
2024-11-25 09:06:03 +0700

    Add files via upload

---
+++ buoi3.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define MAX 6 
+int a[10][10];
+int b[10] = {0};  
+
+void DFS(int v) {
+    b[v] = 1;
+    printf("%d ", v ); 
+   
+    for (int i = 1; i <=10; i++) {
+        if (a[v][i] == 1 && b[i] == 0) {  
+            DFS(i);
+        }
+    }
+}
+
+int main() {
+   
+    FILE *inputFile = fopen("input.txt", "r");
+
+    if (inputFile == NULL) {
+        printf("Khong mo duoc file input.txt\n");
+        return 1;
+    }
+
+    int n;
+    fscanf(inputFile, "%d", &n); 
+
+    
+    for (int i = 1; i <=n; i++) {
+        for (int j = 1; j <=n; j++) {
+            fscanf(inputFile, "%d", &a[i][j]);
+        }
+    }
+
+    
+    fclose(inputFile);
+
+    int x;
+    printf("Nhap dinh can duyet x = ");
+    scanf("%d",&x);
+    printf("Ket qua duyet DFS(1) la: ");
+    DFS(x); 
+    printf("\n");
+
+    return 0;
+}
+
