dd2e89fe6c5ddb73dc8260411d7d178f0d7de5d9
CHUNGMYLINH875 <147479193+CHUNGMYLINH875@users.noreply.github.com>
2024-11-25 09:38:50 +0700

    Add files via upload

--- TH BUOI 3.cpp
+++ TH BUOI 3.cpp
@@ -1,47 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX 6 
-int a[10][10];
+#define MAX 6
+int a[10][10];  
 int b[10] = {0};  
 
-void DFS(int v) {
-    b[v] = 1;
-    printf("%d ", v ); 
-   
-    for (int i = 1; i <=10; i++) {
-        if (a[v][i] == 1 && b[i] == 0) {  
-            DFS(i);
+void DFS(int v, int n) {
+    int stack[10];  
+    int top = -1;  
+
+    
+    stack[++top] = v;
+    b[v] = 1;  
+
+    printf("%d ", v);
+
+    while (top >= 0) {
+        int u = stack[top]; 
+        int found = 0; 
+
+        for (int i = 1; i <= n; i++) {
+            if (a[u][i] == 1 && b[i] == 0) { 
+                stack[++top] = i; 
+                b[i] = 1; 
+                printf("%d ", i);  
+                found = 1;  
+                break; 
+            }
+        }
+
+        if (!found) {
+            top--;  
         }
     }
 }
 
 int main() {
-   
     FILE *inputFile = fopen("1.txt", "r");
 
     if (inputFile == NULL) {
-        printf("File khong mo duoc input.txt\n");
+        printf("Khong mo duoc file input.txt\n");
         return 1;
     }
 
     int n;
-    fscanf(inputFile, "%d", &n); 
+    fscanf(inputFile, "%d", &n);  
 
-    
-    for (int i = 1; i <=n; i++) {
-        for (int j = 1; j <=n; j++) {
+   
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
             fscanf(inputFile, "%d", &a[i][j]);
         }
     }
 
-    
     fclose(inputFile);
 
     int x;
     printf("Nhap dinh can duyet x = ");
-    scanf("%d",&x);
-    printf("Ket qua duyet DFS(1) la: ");
-    DFS(x); 
+    scanf("%d", &x);
+
+    printf("Ket qua duyet DFS la: ");
+    DFS(x, n);
     printf("\n");
 
     return 0;
